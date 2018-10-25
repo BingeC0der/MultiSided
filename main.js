5d4643f7667002c79663735a58fd1756a16e22e7
Nick Johnson <nick.orion2003@gmail.com>
2018-10-25 21:14:37 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function auth() {
+    var name = document.getElementById("aUser").value;
+    var pass = document.getElementById("aPass").value;
+    if (name == "Savage" && pass == "test") {
+        alert("Yes");
+    }
+}
